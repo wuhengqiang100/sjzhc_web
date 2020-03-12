a48c36ec22b05c01af1401c4844a6f4c35ef8a6b
wuhengqiang <wuhengqiang@cbpm-kexin.com>
2020-03-12 15:25:35 +0800

    登陆用户功能基本搞定,下面写角色管理页面设计

---
+++ src/api/loginUser.js
@@ -0,0 +1,68 @@
+import request from '@/utils/request'
+
+export function fetchList(query) {
+  return request({
+    url: '/loginUser/list',
+    method: 'get',
+    params: query,
+    baseURL: 'http://127.0.0.1:8088'
+  })
+}
+
+export function updateUseFlag(id) {
+  return request({
+    url: '/loginUser/updateUseFlag',
+    method: 'post',
+    params: { id },
+    baseURL: 'http://127.0.0.1:8088'
+  })
+}
+
+export function createLoginUser(data) {
+  return request({
+    url: '/loginUser/create',
+    method: 'post',
+    data,
+    baseURL: 'http://127.0.0.1:8088'
+  })
+}
+
+export function updateLoginUser(data) {
+  return request({
+    url: '/loginUser/update',
+    method: 'post',
+    data,
+    baseURL: 'http://127.0.0.1:8088'
+  })
+}
+export function deleteLoginUser(id) {
+  return request({
+    url: '/loginUser/delete',
+    method: 'post',
+    params: { id },
+    baseURL: 'http://127.0.0.1:8088'
+  })
+}
+
+export function fetchLoginUserTypeList() {
+  return request({
+    url: '/loginUserType/listOption',
+    method: 'get',
+    baseURL: 'http://127.0.0.1:8088'
+  })
+}
+
+export function fetchPv(pv) {
+  return request({
+    url: '/loginUser/pv',
+    method: 'get',
+    params: { pv }
+  })
+}
+export function fetchLoginUser(id) {
+  return request({
+    url: '/loginUser/detail',
+    method: 'get',
+    params: { id }
+  })
+}
--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/user/login',
+    url: '/common/login',
     method: 'post',
     data,
     baseURL: 'http://127.0.0.1:8088'
@@ -11,7 +11,7 @@ export function login(data) {
 
 export function getInfo(token) {
   return request({
-    url: '/user/info',
+    url: '/common/info',
     method: 'get',
     params: { token },
     baseURL: 'http://127.0.0.1:8088'
